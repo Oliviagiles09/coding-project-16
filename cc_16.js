70f5097eb7b3567c368f00e32d794cf29b188d78
Oliviagiles09 <giles11@usf.edu>
2025-03-30 22:47:47 -0400

    task 3 - Fetch Products with async/await

--- cc_16.js
+++ cc_16.js
@@ -12,3 +12,14 @@ function fetchProductsThen() {
     handleError(error);
   });
 }
+// Task 3 
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch('https://www.course-api.com/javascript-store-products');
+    const products = await response.json();
+    console.log('Products fetched using async/await:');
+    displayProducts(products);
+  } catch (error) {
+   handleError(error);
+  }
+}
